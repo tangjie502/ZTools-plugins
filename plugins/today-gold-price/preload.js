8f4476657fc0f828bfb0b70709f201d3d392d011
GIWTO <3033291479@qq.com>
2026-03-03 16:26:30 +0800

    Initial commit

---
+++ plugins/today-gold-price/preload.js
@@ -0,0 +1,14 @@
+/**
+ * ZTools Preload Script
+ * 可以在此文件内调用 nodejs、 electron 提供的 api。
+ */
+
+// 例如：暴露基础的 fs 和 path 模块（如果需要）
+// const fs = require("node:fs");
+// const path = require("node:path");
+
+// window.services = {
+//   // 在这里添加你的自定义 API
+// };
+
+console.log('ZTools Gold Price Plugin Preload Loaded');
